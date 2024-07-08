c92f488a5122ca60134cf1aa5077d2d8248fbbb1
zach <zaclewis@gmail.com>
2024-07-08 18:56:30 -0500

    c

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink  } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { twilioAuth } from '../services/auth';
 import { useNavigate } from 'react-router-dom';
 import { Navbar, Nav, Container, Button, Image } from 'react-bootstrap';
@@ -14,35 +14,29 @@ export default function Layout({ children }) {
 
   return (
     <div class="container justify-content-center">
-       <Image 
-            src="/logo.PNG" 
-            height='180px'
-            width='458px'    
-          />
-      <Navbar bg="light" expand="lg" className="custom-navbar">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              {twilioAuth.isAuthenticated ? (
-                <>
-                  <NavLink className="nav-link" to="/">Home</NavLink>
-                  <NavLink className="nav-link"  to="/laundryservice">Laundry Service</NavLink>
-                  <NavLink className="nav-link" to="/caddieservice">Become a Caddie</NavLink>
-                </>
-              ) : null}
-            </Nav>
+      <div class="container">
+        <center><Image
+          src="/logo.PNG"
+          height='180px'
+          width='458px'
+        /></center>
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom" >
+          <ul class="nav nav-pills">
+            <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Home</a></li>
             {twilioAuth.isAuthenticated ? (
-              <Button variant="outline-primary" onClick={handleSignOut}>Sign out</Button>
+              <li class="nav-item"><a href="/login" class="nav-link">SignOut</a></li>
             ) : (
-              <NavLink className="nav-link"  as={Link} to="/login">Login</NavLink>
+              <li class="nav-item"><a href="/login" class="nav-link">Schedule a Caddie</a></li>
             )}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Container  className="mt-4">
+            <li class="nav-item"><a href="#pricing" class="nav-link">Pricing</a></li>
+            <li class="nav-item"><a href="#faqs" class="nav-link">FAQs</a></li>
+          </ul>
+        </header>
+      </div>
+      <center>      <Container className="mt-4 mainback">
         {children}
       </Container>
+      </center>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,59 @@
-import { Container, Row, Col , Image , Button} from 'react-bootstrap';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <div class='container bg-transparent'  >
-      <Container >
-        <Row>
-          
-          <Col>
-            <h1>Welcome to the Soapbox Caddie</h1>
-            <p>Choose Peace. Not Laundry.</p>
-           
-            <hr/>
-          </Col>
-          <hr/>
-          <Image 
-            src="/bannerworks.PNG"     
-          />
-        </Row>
-      </Container>
-    </div>
-  );
+    <>
+      <div class="album py-5 bg-body-tertiary">
+        <div class="topcontainer">
+
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col">
+              <div class="card shadow-sm">
+
+                <center><img src="/public/caddie.PNG" width="200" height="300" /></center>
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Schedule a Caddie</button>
+                    </div>
+                    <small class="text-body-secondary">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <center> <img src="/public/deliver.PNG" width="200" height="300" /></center>
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Schedule a Caddie</button>
+                    </div>
+                    <small class="text-body-secondary">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <center><img src="/public/bag.PNG" width="200" height="300" /></center>
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Schedule a Caddie</button>
+                    </div>
+                    <small class="text-body-secondary">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </>);
 }
